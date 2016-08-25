b7ee8656e533b18d5bb94880f23d092372242a80
Christian Paul <christiansamuelpaul@gmail.com>
2016-08-25 15:30:25 -0400

    Set up public folder and set up socket.io

--- server.js
+++ server.js
@@ -2,9 +2,11 @@ var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io').listen(server);
+var path = require('path');
 var port = process.env.port || 3000
-users = [];
-connections = [];
+
+
+app.use('/public', express.static(path.join(__dirname, 'public')));
 
 server.listen(port, function(req, res) {
 	console.log('listening on ' + port);
@@ -12,4 +14,19 @@ server.listen(port, function(req, res) {
 
 app.get('/', function(req, res) {
 	res.sendFile(__dirname + '/index.html');
+});
+
+
+users = [];
+connections = [];
+io.sockets.on('connection', function(socket) {
+	connections.push(socket);
+	console.log('Connected: %s sockets connected', connections.length);
+
+	// disconnect
+	socket.on('disconnect', function(data) {
+		connections.splice(connections.indexOf(socket), 1);
+		console.log('Disconnected %s sockets connected', connections.length);
+	});
+
 });
