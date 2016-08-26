52962b46bb430b1fb764813a2ee5766066f844eb
Christian Paul <christiansamuelpaul@gmail.com>
2016-08-25 21:49:46 -0400

    Added user interface where users can add new stocks and rerender chart

--- public/scripts/chart-init.js
+++ public/scripts/chart-init.js
@@ -1,69 +1,67 @@
-var seriesOptions = [],
-    seriesCounter = 0,
-    names = ['AAPL', 'GOOG'];
+function setupAndMakeChart(names) {
+  var seriesOptions = [],
+      seriesCounter = 0;
 
-/**
- * Create the chart when all data is loaded
- * @returns {undefined}
- */
-function createChart() {
+  /**
+   * Create the chart when all data is loaded
+   * @returns {undefined}
+   */
+  function createChart() {
 
-    $('#container').highcharts('StockChart', {
-        rangeSelector: {
-            selected: 4
-        },
+      $('#container').highcharts('StockChart', {
+          rangeSelector: {
+              selected: 4
+          },
+          yAxis: {
+              labels: {
+                  formatter: function () {
+                      return (this.value > 0 ? ' + ' : '') + this.value + '%';
+                  }
+              },
+              plotLines: [{
+                  value: 0,
+                  width: 2,
+                  color: 'silver'
+              }]
+          },
+          plotOptions: {
+              series: {
+                  compare: 'percent'
+              }
+          },
+          tooltip: {
+              pointFormat: '<span style="color:{series.color}">{series.name}</span>: <b>{point.y}</b> ({point.change}%)<br/>',
+              valueDecimals: 2
+          },
+          series: seriesOptions
+      });
+  }
 
-        yAxis: {
-            labels: {
-                formatter: function () {
-                    return (this.value > 0 ? ' + ' : '') + this.value + '%';
-                }
-            },
-            plotLines: [{
-                value: 0,
-                width: 2,
-                color: 'silver'
-            }]
-        },
+  $.each(names, function (i, name) {
+      $.getJSON('/api/' + name,    function (data) {
 
-        plotOptions: {
-            series: {
-                compare: 'percent'
-            }
-        },
+      	var parsedData = data.dataset.data.map(function(item) {
+  			item.splice(0, 1, Date.parse(item[0]));
+      		return item;
+      	});
 
-        tooltip: {
-            pointFormat: '<span style="color:{series.color}">{series.name}</span>: <b>{point.y}</b> ({point.change}%)<br/>',
-            valueDecimals: 2
-        },
+          seriesOptions[i] = {
+              name: name,
+              data: parsedData
+          };
 
-        series: seriesOptions
-    });
+          // As we're loading the data asynchronously, we don't know what order it will arrive. So
+          // we keep a counter and create the chart when all the data is loaded.
+          seriesCounter += 1;
+
+          if (seriesCounter === names.length) {
+              createChart();
+          }
+      });
+  });
 }
 
-$.each(names, function (i, name) {
-    $.getJSON('/api/' + name,    function (data) {
-
-    	var parsedData = data.dataset.data.map(function(item) {
-			item.splice(0, 1, Date.parse(item[0]));
-    		return item;
-    	});
-
-        seriesOptions[i] = {
-            name: name,
-            data: parsedData
-        };
-
-        // As we're loading the data asynchronously, we don't know what order it will arrive. So
-        // we keep a counter and create the chart when all the data is loaded.
-        seriesCounter += 1;
-
-        if (seriesCounter === names.length) {
-            createChart();
-        }
-    });
-});
-
+// set style for highcharts
 Highcharts.createElement('link', {
    href: 'https://fonts.googleapis.com/css?family=Dosis:400,600',
    rel: 'stylesheet',
--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,1 +1,16 @@
-// var socket = io.connect();
+var socket = io.connect();
+var stockNames = ['GOOGL', 'AAPL'];
+setupAndMakeChart(stockNames);
+
+var userInput = $('.user-input');
+var submitButton = $('.submit-button');
+
+submitButton.click(function(e) {
+	e.preventDefault();
+
+	if(userInput.val()) {
+		stockNames.push(userInput.val().toUpperCase());
+		userInput.val('');
+		setupAndMakeChart(stockNames);
+	}
+});
