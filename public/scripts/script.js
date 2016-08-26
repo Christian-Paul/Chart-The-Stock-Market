6fcfe2482e52f90ff93e06f5787ccbe02c51a9e3
Christian Paul <christiansamuelpaul@gmail.com>
2016-08-25 23:52:52 -0400

    Separated functionality between client and server, connected them with sockets

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -3,38 +3,37 @@ var submitButton = $('.submit-button');
 var stocksContainer = $('.stocks-container');
 var stock = $('.stock');
 var socket = io.connect();
-var stockNames = ['GOOGL', 'AAPL'];
-setupAndMakeChart(stockNames);
-buildStockBoard(stockNames);
 
+// emits an event and sends server the name of the stock to be added
 submitButton.click(function(e) {
-
 	e.preventDefault();
 
 	if(userInput.val()) {
-		stockNames.push(userInput.val().toUpperCase());
+		var stockName = userInput.val().toUpperCase();
 		userInput.val('');
-		setupAndMakeChart(stockNames);
-		buildStockBoard(stockNames);
-	}
+		socket.emit('add stock', stockName);
+	};
 
 });
 
+// emits an event and sends server the name of the stock to be removed
 stocksContainer.on('click', stock, function(e) {
 	var stockName = $(e.target).text();
 
-
-	stockNames.splice(stockNames.indexOf(stockName), 1);
-	setupAndMakeChart(stockNames);
-	buildStockBoard(stockNames);
+	socket.emit('remove stock', stockName);
 });
 
-function buildStockBoard(names) {
+// server emits this event when it updates the list of tracked stocks
+socket.on('update board', function(data) {
+	setupAndMakeChart(data);
+	buildStockBoard(data);
+});
 
+// clears and rebuilds the list of tracked stocks in the DOM
+function buildStockBoard(names) {
 	stocksContainer.empty();
 
 	for(var i = 0; i < names.length; i++) {
 		stocksContainer.append('<div class="stock">' + names[i] + '</div>');
 	}
-
 };
--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var path = require('path');
 var https = require('https');
 var config = require('./config.js');
 var port = process.env.port || 3000
-
+var stockList = ['GOOG', 'AAPL'];
 
 app.use('/public', express.static(path.join(__dirname, 'public')));
 
@@ -18,6 +18,8 @@ app.get('/', function(req, res) {
 	res.sendFile(__dirname + '/index.html');
 });
 
+
+// requests for stock data are handled through server so credentials remain hidden
 app.get('/api/:tagId', function(req, res) {
 	var stockName = req.params.tagId;
 	var dest = 'https://www.quandl.com/api/v3/datasets/WIKI/' + stockName + '.json?column_index=1&order=asc&start_date=2010-01-01&api_key=' + config.quandlKey;
@@ -25,17 +27,35 @@ app.get('/api/:tagId', function(req, res) {
 	res.redirect(dest);
 });
 
-
-users = [];
 connections = [];
 io.sockets.on('connection', function(socket) {
 	connections.push(socket);
 	console.log('Connected: %s sockets connected', connections.length);
 
-	// disconnect
+	// when a user connects, emit this event to only them
+	// renders their chart and board for the first time
+	io.to(socket.id).emit('update board', stockList);
+
+	// when user disconnects, remove them from connections array
 	socket.on('disconnect', function(data) {
 		connections.splice(connections.indexOf(socket), 1);
 		console.log('Disconnected %s sockets connected', connections.length);
 	});
 
+	// when a stock is added, push it to the stock list and update board
+	socket.on('add stock', function(data) {
+		stockList.push(data);
+		updateBoard();
+	});
+
+	// when a stock is removed, remove it from the stock list and update board
+	socket.on('remove stock', function(data) {
+		stockList.splice(stockList.indexOf(data), 1);
+		updateBoard();
+	});
+
+	// update board
+	function updateBoard() {
+		io.sockets.emit('update board', stockList);
+	};
 });
