0c6575aa5a14846ef9aaa3de536a053c526b484f
Christian Paul <christiansamuelpaul@gmail.com>
2016-08-25 22:16:52 -0400

    Users can now click on a stock name to remove it from tracking and rerender chart

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,16 +1,40 @@
+var userInput = $('.user-input');
+var submitButton = $('.submit-button');
+var stocksContainer = $('.stocks-container');
+var stock = $('.stock');
 var socket = io.connect();
 var stockNames = ['GOOGL', 'AAPL'];
 setupAndMakeChart(stockNames);
-
-var userInput = $('.user-input');
-var submitButton = $('.submit-button');
+buildStockBoard(stockNames);
 
 submitButton.click(function(e) {
+
 	e.preventDefault();
 
 	if(userInput.val()) {
 		stockNames.push(userInput.val().toUpperCase());
 		userInput.val('');
 		setupAndMakeChart(stockNames);
+		buildStockBoard(stockNames);
 	}
+
 });
+
+stocksContainer.on('click', stock, function(e) {
+	var stockName = $(e.target).text();
+
+
+	stockNames.splice(stockNames.indexOf(stockName), 1);
+	setupAndMakeChart(stockNames);
+	buildStockBoard(stockNames);
+});
+
+function buildStockBoard(names) {
+
+	stocksContainer.empty();
+
+	for(var i = 0; i < names.length; i++) {
+		stocksContainer.append('<div class="stock">' + names[i] + '</div>');
+	}
+
+};
